d74e1b03afa94d09d76c93e2ef5b6502d9fa8042
ppomarswfs <pere.pomar@smallworldfs.com>
2023-04-18 12:07:14 +0200

    TransactionDataServiceClient getTransaction

--- src/main/java/com/smallworldfs/transactionservice/transaction/client/TransactionDataServiceClient.java
+++ src/main/java/com/smallworldfs/transactionservice/transaction/client/TransactionDataServiceClient.java
@@ -1,8 +1,14 @@
 package com.smallworldfs.transactionservice.transaction.client;
 
 import com.smallworldfs.transactionservice.transaction.entity.Transaction;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@FeignClient(name = "transaction-data-service", url = "${transaction-data-service.url}")
 public interface TransactionDataServiceClient {
 
-    Transaction getTransaction(Integer id);
+    @GetMapping(value = "/transactions/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    Transaction getTransaction(@PathVariable Integer id);
 }
--- src/test/java/com/smallworldfs/transactionservice/TransactionServiceApplicationTests.java
+++ src/test/java/com/smallworldfs/transactionservice/TransactionServiceApplicationTests.java
@@ -1,18 +1,12 @@
 package com.smallworldfs.transactionservice;
 
-import com.smallworldfs.transactionservice.transaction.client.TransactionDataServiceClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SpringBootTest(properties = "transaction-data-service.url=http://localhost:8080")
 class TransactionServiceApplicationTests {
 
-    @MockBean
-    private TransactionDataServiceClient client;
-
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 
 }
